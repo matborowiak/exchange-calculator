45f5daa99e9540208f8756218c72130266841ac2
matborowiak <m.borowiak@hotmail.com>
2020-11-11 20:40:18 +0100

    upd fetchRates.js to validate fetched data with user selected currencies

--- src/functions/fetchRates.js
+++ src/functions/fetchRates.js
@@ -1,15 +1,40 @@
 import axios from 'axios'
 
-const fetchRates = async () => {
+const fetchRates = async (have, get, base) => {
   return await axios
     .post(
       'https://openexchangerates.org/api/latest.json?app_id=0c609280a3334d389f61bc9201c8f459&symbols=GBP,EUR'
     )
     .then((response) => {
-      return response.data.rates
+      const data = response.data.rates
+      //validate
+      if (
+        (base === have && data[get.toUpperCase()]) ||
+        (base === get && data[have.toUpperCase()]) ||
+        (data[have.toUpperCase()] && data[get.toUpperCase()])
+      ) {
+        return data
+      } else {
+        throw new Error('Received wrong server data!')
+      }
     })
     .catch((err) => console.log(err))
+  // // mock for tests
   // return { EUR: 0.8536, GBP: 0.769389 }
+  // const response = { data: { rates: { GBP: 0.769389 } } }
+  // const data = response.data.rates
+  // //validate
+  // if (
+  //   (base === have && data[get.toUpperCase()]) ||
+  //   (base === get && data[have.toUpperCase()]) ||
+  //   (data[have.toUpperCase()] && data[get.toUpperCase()])
+  // ) {
+  //   console.log('RETURNING DATA')
+  //   return data
+  // } else {
+  //   console.log('THROWING ERROR')
+  //   throw { error: 'Received wrong server data!' }
+  // }
 }
 
 export default fetchRates
